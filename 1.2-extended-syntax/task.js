eb387524fe26cd380fe3756755431fd3dcfeb7c8
Dmitry Tsyguro <noisydimon@yandex.ru>
2020-08-06 18:34:21 +0300

    3rd hometask

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,5 +1,12 @@
+"use strict"
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
+   a * x * b + 4 * x - c == 0;
+   D = b**2 - 4 * a * c;
+    if (D < o) { x {}
+} else if (D == 0) {x = (- b) / 2 * a
+} else {x1 = ((- b) + Math.sqrt(b ** 2 - 4 * a * c) / 2 * a),
+x2 = ((- b) + Math.sqrt(b ** 2 + 4 * a * c) / 2 * a) }    // код для задачи №1 писать здесь
     // return x;
 }
 
--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,38 @@
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    if (!Number(percent)) {console.log('Параметр percent содержит неправильное значение ' + percent + '.')}
+    if (!Number(contribution)) {console.log('Параметр percent содержит неправильное значение ' + contribution + '.')}
+    if (!Number(amount)) {console.log('Параметр percent содержит неправильное значение ' + amount + '.')}
+    if (!Date.parse(date)) {console.log('Параметр date содержит неправильное значение ' + date + '.')}
+
+let creditBody = amount - contribution//тело кредита
+
+let d1 = new Date(date);   
+let d2 = new Date();
+let creditMonths = d1.getMonth() - d2.getMonth()
++ (12 * (d1.getFullYear() - d2.getFullYear()))
+//кол-во выплачиваемых месяцев
+
+let P = percent / 100 * 12; 
+let monthlyPayment = creditBody * (P + P / ((( 1 + P) ** creditMonths) - 1)) //ежемесячная оплата
+
+let totalAmount = contribution + monthlyPayment * creditMonths //общая сумма
+
+let finalTotalAmount = Math.floor(totalAmount * 100) / 100  //округлить результат
+
+console.log("Ввод: " + percent * 100 + "," + contribution + "," + amount + "," + creditMonths + ". Вывод: " + finalTotalAmount)
+}
+
+calculateTotalMortgage(0.05, 2300, 43000, '2021.10.4')
+
+
     // код для задачи №1 писать здесь
     // return totalAmount;
 }
 
-function getGreeting(name) {
+function getGreeting(name = "Аноним") {
+
+    let greeting = 'Привет, мир! Меня зовут ' + name + '.';
     // код для задачи №2 писать здесь
-    // return greeting;
+     return greeting;
 }
