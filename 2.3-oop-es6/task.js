9f5998cc345ba2f7d8dfb23da5efaa57c99f266f
Dmitry Tsyguro <noisydimon@yandex.ru>
2020-08-24 22:38:42 +0300

    version 1

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -70,3 +70,68 @@ console.log(picknick.state); //10
 
 picknick.fix();
 console.log(picknick.state); //15
+
+
+
+//Задача 2
+
+class Library {
+  constructor (libraryName, books) {
+  this.libraryName = libraryName;
+  this.books = [];
+  }
+  addbook(book){
+    if(this.book.state > 30) {
+      this.books.push(book.bookName);
+    }
+  }
+  
+  findBookBy(type, value) {
+  for (let i = 0; i < this.books.length; i++) {
+    for (let prop in this.books[i]) {
+      if (prop ===type && this.books[i]) {        
+          return this.books[i].name;
+        } else {
+          return null
+          };
+      }
+    }
+  }
+   giveBookByName(bookName){
+    for (let i = 0; i < this.books.length; i++){
+      if (this.books[i].name === bookName){
+        this.books.splice([i],1);
+        console.log(`Удаленная книга  ${bookName} ${this.books[i]}`);
+        return this.books[i];
+      } else {
+        return null
+        };
+      }
+   }
+}
+
+
+  //Задача 3
+
+  /*class StudentLog {
+    constructor(studentName) {
+      this.studentName = studentName;
+  }
+  
+  getName(){
+    return this.studentName
+  }
+  addGrade(grade, subject){
+    if (grade !== 1 || 2 || 3 || 4 || 5) {
+      return `Вы пытались поставить оценку ${grade} по предмету ${subject}. Допускаются только числа от 1 до 5.`
+    }
+  }
+  }
+  
+
+  //Проверки
+  const log = new StudentLog('Олег Никифоров');
+  console.log(log)
+  
+  log.getName()
+  log.addGrade(23, `math`)
