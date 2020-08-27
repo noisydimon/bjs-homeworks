aad6948ea1761de628e733637af54212383b39d4
Dmitry Tsyguro <noisydimon@yandex.ru>
2020-08-27 05:29:32 +0300

    version3

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -86,7 +86,7 @@ class Library {
   giveBookByName(bookName){
     for(let i = 0;i < this.books.length;i++){
       if(this.books[i].name === bookName){
-        return this.books.splice([i],1);
+        return this.books.splice([i],1)[0];
       }
     }
     return null;
